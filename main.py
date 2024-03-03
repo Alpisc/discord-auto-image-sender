69228d1b27b7a3a87d40219a885bc627ad95928a
Alpisc <73418528+Alpisc@users.noreply.github.com>
2024-03-03 21:47:45 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+import os
+import time
+
+load_dotenv()
+
+TOKEN = os.environ["TOKEN"]
+delete = bool(os.environ["DELETE"])
+
+client = commands.Bot(command_prefix=">", self_bot=True)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.command()
+async def images(ctx):
+    directory = os.getcwd()
+    amount = 0
+    total =len(os.listdir())
+    for filename in os.listdir(directory+"/Images"):
+        file_path = os.path.join(directory+"/Images", filename)
+        if os.path.isfile(file_path):
+            file = discord.File(file_path, filename)
+            print("Sending: {filename} from {file_path} {total - amount} images remaining")
+            try:
+                await ctx.send(file=file)
+            except Exception:
+                print("Bad image, couldnt send")
+            if delete: os.remove(file_path)
+            amount = amount + 1
+            time.sleep(5)
+    print(f"Done! send {amount} Images")
+            
+
+client.run(TOKEN)
